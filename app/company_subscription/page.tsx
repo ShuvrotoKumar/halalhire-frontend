712030d5858ccf1024b96f00b51c92a3b20f9a05
ShuvrotoKumar <sdks6409@gmail.com>
2026-04-19 01:57:42 +0600

    feat: add company subscription page component

---
+++ app/company_subscription/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import CompanySubscription from '@/components/company_subscription/company_subscription';
+
+export default function CompanySubscriptionPage() {
+  return <CompanySubscription />;
+}
